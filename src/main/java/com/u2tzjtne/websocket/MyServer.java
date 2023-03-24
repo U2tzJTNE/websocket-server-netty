1ade7af017e1d446420d9b67a0f3e3c84fa98187
Luke <u2tzjtne@gmail.com>
2023-03-24 12:02:15 +0800

    优化

--- src/main/java/com/u2tzjtne/websocket/MyServer.java
+++ src/main/java/com/u2tzjtne/websocket/MyServer.java
@@ -1,17 +1,14 @@
 package com.u2tzjtne.websocket;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
-import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * @Author u2tzjtne@gmail.com
@@ -21,29 +18,32 @@ import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketSe
 public class MyServer {
     public static void main(String[] args) throws InterruptedException {
         // 创建两个线程组
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.option(ChannelOption.SO_BACKLOG, 1024);
             bootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     // 在workerGroup中增加处理器
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) {
+                            System.out.println("收到新连接:" + ch.localAddress());
                             ChannelPipeline pipeline = ch.pipeline();
                             //HTTP 协议解析，用于握手阶段
                             pipeline.addLast(new HttpServerCodec());
+                            pipeline.addLast(new ChunkedWriteHandler());
                             //HTTP 协议解析，用于握手阶段
-                            pipeline.addLast(new HttpObjectAggregator(65536));
+                            pipeline.addLast(new HttpObjectAggregator(8192));
                             //WebSocket 数据压缩扩展
-                            pipeline.addLast(new WebSocketServerCompressionHandler());
+//                            pipeline.addLast(new WebSocketServerCompressionHandler());
                             /*
                               对应的webSocket，它的数据是以帧（frame）的形式传递的。
                               浏览器发送webSocket请求时，使用ws协议，如 ws://localhost:1234/hello。
                               WebSocketServerProtocolHandler 核心功能是将http协议升级为ws协议，保持长连接
                              */
-                            pipeline.addLast(new WebSocketServerProtocolHandler("/socket", null, true));
+                            pipeline.addLast(new WebSocketServerProtocolHandler("/socket", "WebSocket", true, 65536 * 10));
                             // 自定义handler，处理业务逻辑
                             pipeline.addLast(new MyServerHandler());
                         }
@@ -52,8 +52,8 @@ public class MyServer {
             ChannelFuture channelFuture = bootstrap.bind(8888).sync();
             channelFuture.channel().closeFuture().sync();
         } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully().sync();
+            workerGroup.shutdownGracefully().sync();
         }
     }
 }
--- src/main/java/com/u2tzjtne/websocket/MyServerHandler.java
+++ src/main/java/com/u2tzjtne/websocket/MyServerHandler.java
@@ -1,8 +1,13 @@
 package com.u2tzjtne.websocket;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 /**
  * @Author u2tzjtne@gmail.com
  * @Description Netty WebSocket长连接处理类
@@ -10,33 +15,42 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
  */
 public class MyServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
-    /**
-     * TextWebSocketFrame 表示一个文本帧（frame）
-     */
+    public static ChannelGroup channelGroup;
+
+    static {
+        channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    }
+
+    //客户端与服务器建立连接的时候触发，
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println(ctx.channel().id() + "建立连接");
+        //添加到channelGroup通道组
+        channelGroup.add(ctx.channel());
+    }
+
+    //客户端与服务器关闭连接的时候触发，
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println(ctx.channel().id() + "断开连接");
+        channelGroup.remove(ctx.channel());
+    }
+
+    //服务器接受客户端的数据信息，
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) {
-        System.out.println("服务器收到消息 ==> " + msg.text());
-        // 返回数据  LocalDateTime.now()表示获取当前时间
-        ctx.writeAndFlush(new TextWebSocketFrame(msg.text()));
+        System.out.println("服务器收到的数据：" + msg.text());
+        //sendMessage(ctx);
+        sendAllMessage(ctx.channel(), msg.text());
     }
 
-    @Override
-    public void handlerAdded(ChannelHandlerContext ctx) {
-        // 获取channel唯一值，asLongText方法是channel的id的全名
-        System.out.println("handlerAdded被调用 " + ctx.channel().id().asLongText());
-        // asShortText方法是获取channel的id短名，是全名末尾一段的截取
-        System.out.println("handlerAdded被调用 " + ctx.channel().id().asShortText());
+    //给固定的人发消息
+    private void sendMessage(Channel channel, String message) {
+        channel.writeAndFlush(new TextWebSocketFrame(message));
     }
 
-    @Override
-    public void handlerRemoved(ChannelHandlerContext ctx) {
-        System.out.println("handlerRemoved被调用 " + ctx.channel().id().asLongText());
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        System.out.println("异常发生 ");
-        cause.printStackTrace();
-        ctx.channel().close();
+    //发送群消息,此时其他客户端也能收到群消息
+    private void sendAllMessage(Channel channel, String message) {
+        channelGroup.writeAndFlush(new TextWebSocketFrame(message), ch -> ch.id() != channel.id());
     }
 }
